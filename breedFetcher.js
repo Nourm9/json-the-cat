929edb1134d771158896b7c82fb3a8b3e9cec82b
Nour Mohamoud <nour.mohamoud9@gmail.com>
2022-12-19 01:58:39 +0000

    add: index.js, breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const request = require("request");
 
-const arr = process.argv;
-const cat = String(arr[2]);
-let url = "https://api.thecatapi.com/v1/breeds/search?q=" + cat;
-console.log(url);
 
+const fetchBreedDescription = function (breedName, callback) {
+  let url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    console.log(data);
+    const objectLoc = data[0].description;
+    return callback(null, objectLoc);
+  });
+};
 
-
-request(url, (error, response, body) => {
-  console.log("error:", error);
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-});
+module.exports = { fetchBreedDescription };
